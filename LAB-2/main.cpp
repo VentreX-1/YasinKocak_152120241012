2339637ba91fcde9c979fac856c891cbad108bcd
VentreX-1 <ysnkocak03@gmail.com>
2026-03-11 11:56:06 +0300

    LAB-2

---
+++ LAB-2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct node {
+    string data;
+    struct node* next;
+};
+
+void addWord(string word)
+{
+
+}
+
+void undo()
+{
+
+}
+
+void display()
+{
+
+}
+
+int main()
+{
+    string input;
+
+    while (1)
+    {
+        cout << "Input: ";
+        cin >> input;
+
+        if (input == "EXIT")
+        {
+            break;
+        }
+        else if (input == "UNDO")
+        {
+
+        }
+        else
+        {
+            addWord(input);
+        }
+
+        display();
+    }
+
+    return 0;
+}
